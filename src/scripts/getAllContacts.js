c007cd55d1b303968ac91052a59f85c390684c5f
sofia11244 <denizsofia03@gmail.com>
2025-01-14 14:10:31 +0300

    get all contact code

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
 
-console.log(await getAllContacts());
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    throw error;
+  }
+};
+
+getAllContacts().then(contacts => console.log(contacts));
