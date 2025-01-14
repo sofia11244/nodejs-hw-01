878cb5b9df68de802bcd959ac9ad2a55788a1868
sofia11244 <denizsofia03@gmail.com>
2025-01-14 13:51:46 +0300

    contatc generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,20 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+try {
+    const contacts = await readContacts();
+    for (let i = 0; i < number; i += 1) {
+        contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+
+    console.log(`Generated ${number} contacts`);
+} catch (error) {
+    console.error(error);
+    throw error;
+}
+};
 
 generateContacts(5);
