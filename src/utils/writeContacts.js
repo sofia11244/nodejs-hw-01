e1384c6b2bc0084f9c3680a60a38ffb069295900
sofia11244 <denizsofia03@gmail.com>
2025-01-14 13:33:51 +0300

    .

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts successfully updated!');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw error;
+  }
+};
